5d7fda97df9d2ec4a0c94b52d77b76e9ff84238b
emergy <emergy@docker.lan>
2023-12-12 23:53:04 +0600

    followRedirects

--- app.js
+++ app.js
@@ -11,20 +11,47 @@ const config = process.env.CONFIG ? JSON.parse(process.env.CONFIG) : {
   'myip.lan'       : 'http://ifconfig.me'
 };
 
+console.log('Config:', config);
+
 const app = express();
 
 app.use(helmet());
 app.use('/', createProxyMiddleware({ 
+  target: config.default,
   router: (req) => {
     const host = req.headers.host.split(':')[0];
-    console.log(req.originalUrl);
-    return config[host] ? config[host] : config.default;
+    const targetUrl = config[host] ? config[host] : config.default;
+    // console.log('Original URL:', req.originalUrl);
+    // console.log('Target URL:', targetUrl);
+    // console.log(req);
+    return targetUrl;
   },
   changeOrigin: true,
   autoRewrite: true,
   protocolRewrite: true,
   logger: console,
-  pathRewrite: (path, req) => path.replace(/^\/https?:\/\/[^\/]+/, ''),
+  // logLevel: 'debug',
+  // onError: (err, req, res, target) => {
+  //   console.log('||| Error:', err);
+  //   console.log('||| Req:', req);
+  //   console.log('||| Res:', res);
+  //   console.log('||| Target:', target);
+  // },
+  //pathRewrite: (path, req) => path.replace(/^\/https?:\/\/[^\/]+/, ''),
+  pathRewrite: {
+    '^/https?://[^/]+': '',
+  },
+  protocolRewrite: true,
+  ws: true,
+  // hostRewrite: true,
+  // autoRewrite: true,
+  // protocolRewrite: 'http',
+  followRedirects: true,
+  // on: {
+  //   onProxyReqWs: (proxyReq, req, socket, options, head) => {
+  //     console.log(proxyReq);
+  //   },
+  // }
 }));
 
 app.listen(PORT);
